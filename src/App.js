b1345da443b29f841a574741a5e85d0042f77829
zxalfred <zxsprout@foxmail.com>
2019-03-30 23:21:46 +0800

    rough style of header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import Header from './common/header';
 
 class App extends Component {
   render() {
     return (
       <div>
-        
+        <Header />
       </div>
     );
   }
